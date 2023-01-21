0cf6b89b2b9aa1531e6659a59505f778d3baddf1
Dmitriy Samsonov <LeonSpace777@yandex.ru>
2023-01-21 13:11:39 +0300

    Task1 added

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -4,3 +4,18 @@
 // 782 -> 8
 // 918 -> 1
 
+
+System.Console.WriteLine("Введите трёхзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (99 < num && num < 1000)
+
+{
+    int num1 = (num / 10 % 10);
+    System.Console.Write($"Второй цифрой числа является: ");
+    System.Console.WriteLine(num1);
+}
+else
+{
+    System.Console.WriteLine("Введено не трёхзначное число"); 
+}
